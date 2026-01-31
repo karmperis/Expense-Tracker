b43fc3fd51eb8b5b193dba9046468a2a722f223b
Nikolaos Karmperis <karmperis@gmail.com>
2026-01-31 20:13:10 +0200

    feat: auditing functionality by automatically updating the created_at and updated_at timestamps

---
+++ src/main/java/com/karmperis/expensetracker/model/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.karmperis.expensetracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+/**
+ * This class provides auditing functionality by automatically updating
+ * the created_at and updated_at timestamps when an entity
+ * is created or updated.
+ */
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+    @CreatedDate
+    @Column(name ="created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    private Instant created_at;
+
+    @LastModifiedDate
+    @Column(name ="updated_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant updated_at;
+}
