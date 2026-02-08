55c0fd7076b0c8791cd36be9b429aadf05f0603d
Nikolaos Karmperis <karmperis@gmail.com>
2026-02-08 23:54:37 +0200

    feat: add  transaction type entity docs: update transaction entity documentation

--- src/main/java/com/karmperis/expensetracker/model/Transaction.java
+++ src/main/java/com/karmperis/expensetracker/model/Transaction.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 /**
- * Represents a financial transaction (income or expense) in the database.
+ * Entity: Represents a financial transaction (income or expense) in the database.
  */
 @Entity
 @Table(name = "transactions")
