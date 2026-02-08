4ea32f9edc9f64b2cda5085e5b81012904fb7900
Nikolaos Karmperis <karmperis@gmail.com>
2026-02-08 19:26:23 +0200

    add: category entity

---
+++ src/main/java/com/karmperis/expensetracker/model/Category.java
@@ -0,0 +1,30 @@
+package com.karmperis.expensetracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Represents a category table in the database.
+ */
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category extends AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_type_id", nullable = false)
+    private CategoryType categoryType;
+
+    @Builder.Default
+    private Boolean active = true;
+}
