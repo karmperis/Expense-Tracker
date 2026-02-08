91f6d712d1c66c4d549b31d4c1444748ec5b992b
Nikolaos Karmperis <karmperis@gmail.com>
2026-02-08 19:37:01 +0200

    add: payment method entity

---
+++ src/main/java/com/karmperis/expensetracker/model/PaymentMethod.java
@@ -0,0 +1,27 @@
+package com.karmperis.expensetracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Represents a payment method table in the database.
+ */
+
+@Entity
+@Table(name = "payment_methods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentMethod extends AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String paymentMethod;
+
+    @Builder.Default
+    private Boolean active = true;
+}
