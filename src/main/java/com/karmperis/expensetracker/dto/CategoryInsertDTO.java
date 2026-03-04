1c02edf5730e44ae6dada66e61d2420f30364a42
Nikolaos Karmperis <karmperis@gmail.com>
2026-03-04 18:37:59 +0200

    feat: add dto for create a new category

---
+++ src/main/java/com/karmperis/expensetracker/dto/CategoryInsertDTO.java
@@ -0,0 +1,20 @@
+package com.karmperis.expensetracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO for create a new category from frontend.
+ * @param category The name of the category. Not blank, must contain at least two characters.
+ * @param categoryTypeId The id of the category type. Not null, is required.
+ */
+public record CategoryInsertDTO(
+        @NotBlank(message = "The category cannot be blank.")
+        @Size(min = 2, message = "The category must contain at least two characters.")
+        String category,
+
+        @NotNull(message = "The category type is required.")
+        Long categoryTypeId
+) {
+}
