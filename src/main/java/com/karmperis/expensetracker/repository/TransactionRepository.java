46f656c4deb4a31473ec6b0abbd064116bf96887
Nikolaos Karmperis <karmperis@gmail.com>
2026-03-15 17:32:34 +0200

    refactor: update TransactionRepository to support pageable parameters feat: add method to filter transactions by category type

--- src/main/java/com/karmperis/expensetracker/repository/TransactionRepository.java
+++ src/main/java/com/karmperis/expensetracker/repository/TransactionRepository.java
@@ -1,12 +1,10 @@
 package com.karmperis.expensetracker.repository;
 
-import com.karmperis.expensetracker.model.Account;
-import com.karmperis.expensetracker.model.Category;
-import com.karmperis.expensetracker.model.Transaction;
-import com.karmperis.expensetracker.model.User;
+import com.karmperis.expensetracker.model.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,48 +24,57 @@ public interface TransactionRepository extends JpaRepository <Transaction, Long>
     /**
      * Retrieves all active transactions associated with a specific user.
      * @param user The user entity to filter transactions by.
-     * @return A list of active transactions belonging to the specified user.
+     * @return A page of active transactions belonging to the specified user.
      */
-    List<Transaction> findByUserAndActiveTrueOrderByDateDesc(User user);
+    Page<Transaction> findByUserAndActiveTrueOrderByDateDesc(User user, Pageable pageable);
 
     /**
      * Retrieves active transactions for a specific user within a given date range.
      * @param user The user entity.
      * @param startDate The start date of the range.
      * @param endDate The end date of the range.
-     * @return A list of active transactions within the date range.
+     * @return A page of active transactions within the date range.
      */
-    List<Transaction> findByUserAndActiveTrueAndDateBetweenOrderByDateDesc(User user, LocalDate startDate, LocalDate endDate);
+    Page<Transaction> findByUserAndActiveTrueAndDateBetweenOrderByDateDesc(User user, LocalDate startDate, LocalDate endDate, Pageable pageable);
 
     /**
      * Retrieves active transactions for a specific user and account.
      * @param user The user entity.
      * @param account The account to filter by.
-     * @return A list of active transactions belonging to the specific account.
+     * @return A page of active transactions belonging to the specific account.
      */
-    List<Transaction> findByUserAndAccountAndActiveTrueOrderByDateDesc(User user, Account account);
+    Page<Transaction> findByUserAndAccountAndActiveTrueOrderByDateDesc(User user, Account account, Pageable pageable);
 
     /**
      * Retrieves active transactions for a specific user and category.
      * @param user The user entity.
      * @param category The category to filter by.
-     * @return A list of active transactions belonging to the specific category.
+     * @return A page of active transactions belonging to the specific category.
      */
-    List<Transaction> findByUserAndCategoryAndActiveTrueOrderByDateDesc(User user, Category category);
+    Page<Transaction> findByUserAndCategoryAndActiveTrueOrderByDateDesc(User user, Category category, Pageable pageable);
 
     /**
      * Retrieves all inactive transactions associated with a specific user.
      * @param user The user entity to filter transactions by.
-     * @return A list of inactive transactions belonging to the specified user.
+     * @return A page of inactive transactions belonging to the specified user.
      */
-    List<Transaction> findByUserAndActiveFalseOrderByDateDesc(User user);
+    Page<Transaction> findByUserAndActiveFalseOrderByDateDesc(User user, Pageable pageable);
 
     /**
      * Retrieves inactive transactions for a specific user within a given date range.
      * @param user The user entity.
      * @param startDate The start date of the range.
      * @param endDate The end date of the range.
-     * @return A list of inactive transactions within the date range.
+     * @return A page of inactive transactions within the date range.
      */
-    List<Transaction> findByUserAndActiveFalseAndDateBetweenOrderByDateDesc(User user, LocalDate startDate, LocalDate endDate);
+    Page<Transaction> findByUserAndActiveFalseAndDateBetweenOrderByDateDesc(User user, LocalDate startDate, LocalDate endDate, Pageable pageable);
+
+    /**
+     * Retrieves transactions based on category type.
+     * @param user The owner of the transactions.
+     * @param categoryType The type of category to filter by.
+     * @return A page of active transactions matching the specified category type.
+     */
+    Page<Transaction> findByUserAndCategory_CategoryTypeAndActiveTrueOrderByDateDesc(
+            User user, CategoryType categoryType, Pageable pageable);
 }
